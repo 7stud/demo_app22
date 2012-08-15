9547c07c8bf04a5f75745142d8a53ab4a994f2d0
7stud <7stud@excite.com>
2012-08-15 01:22:14 -0700

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+DemoApp22::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp22::Application.config.secret_token = '237973f19cdb8bd57432eabd622c1fc5e73ab533d22dc9fb67787dec82acece06780ec97c09315519d11daaae95cf88a360717e80722ee4a2205e16804d9e661'
